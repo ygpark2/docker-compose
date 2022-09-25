17628b5dabcd92294ac7cf208b99c1d1d066e416
Young Gyu Park <ygpark2@gmail.com>
2022-09-25 12:43:47 +0900

    create schema log db

--- conf/mongodb/docker-entrypoint-initdb.d/mongo-init.js
+++ conf/mongodb/docker-entrypoint-initdb.d/mongo-init.js
@@ -15,13 +15,14 @@ db.tickets.insert({ "price": 10, "description": "1-Day ticket", "id": "1D" });
 db.tickets.insert({ "price": 20, "description": "2-Day ticket", "id": "2D" });
 
 
-// JUNGTO_DATA DB
-db = new Mongo().getDB("JungtoData");
+// JUNGTO_LOG DATA DB
+db = new Mongo().getDB("JungtoLogData");
 
 db.createUser({
-    user: 'test_user',
-    pwd: 'test1234',
+    user: 'jungto_log_user',
+    pwd: 'jungto2389e[233wp',
     roles: [{ role: 'readWrite', db: 'JungtoData' }],
 });
 
-db.createCollection('JungtoUser');
+// db.createCollection('JungtoUser');
+//
